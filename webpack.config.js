9da7fbb9a887d28e58bed73069287a8550511b5e
Accendit <36697826+Accendit@users.noreply.github.com>
2019-12-23 14:10:14 +0100

    Added Vue loader and added parameters for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,71 +2,90 @@
 const path = require('path');
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = {
 
-    entry: ['./src/scripts/app.ts', './src/styles/main.scss', './src/index.html'],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+module.exports = (env, argv) => {
 
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        hot: true
-    },
+    let isProduction = argv.mode === 'production'
 
-    module: {
-        rules: [
-            {
-                test: /.tsx?$/,
-                loader: 'ts-loader',
-                exclude: [/node_modules/]
-            },
+    return {
 
-            {
-                test: /.html$/,
-                loader: 'html-loader',
-                options: {
-                    minimize: true,
-                    collapseWhitespace: true,
-                    removeComments: true
-                }
-            },
+        entry: ['./src/scripts/app.ts', './src/styles/main.scss', './src/index.html'],
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
+        },
 
-            {
-                test: /.(s*)css$/,
-                use: [
-                    {
-                        loader: miniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    { 
-                        loader: 'sass-loader',
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            hot: true
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: [/node_modules/]
+                },
+
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                    options: {
+                        minimize: isProduction,
+                        collapseWhitespace: isProduction,
+                        removeComments: isProduction
                     }
-                ],
+                },
+
+                {
+                    test: /\.(s*)css$/,
+                    use: [
+                        {
+                            loader: miniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader'
+                        },
+                        { 
+                            loader: 'sass-loader',
+                        }
+                    ],
+                },
+
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    options: {
+                        collapseWhitespace: true,
+                        
+                    }
+                }
+                
+            ]},
+
+        resolve: {
+            extensions: [ '.ts', '.tsx', '.js' ],
+            alias: {
+                vue: 'vue/dist/vue.js'
             }
-            
-        ]},
+        },
 
-    resolve: {
-        extensions: [ '.ts', '.tsx', '.js' ],
-        alias: {
-            vue: 'vue/dist/vue.js'
-        }
-    },
+        plugins: [
+            new miniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+                ignoreOrder: false
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: './src/index.html'
+            }),
+            new VueLoaderPlugin
+        ]
 
-    plugins: [
-        new miniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: false
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
-    ]
+    }
+
 };
